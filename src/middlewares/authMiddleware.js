709b1b12d0e8a285c6983f273768af884d433012
Theo <jaybuck403@gmail.com>
2024-09-06 23:33:53 +0000

    fixed id

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,15 +1,15 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = function (req, res, next) {
-  const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied');
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).send("Access denied");
 
   try {
-    const decoded = jwt.verify(token, 'jwtPrivateKey');				// set privateKey in .env file
+    const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY); // set privateKey in .env file
     req.user = decoded;
     next();
+  } catch (error) {
+    res.status(400).send("Invalid token");
   }
-  catch (error) {
-    res.status(400).send('Invalid token');
-  }
-}
+};
